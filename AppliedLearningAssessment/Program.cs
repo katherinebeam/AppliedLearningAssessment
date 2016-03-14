b3777f1fe82b929e95963ff112e68fae95fd870c
lterrian <lterrian@gmail.com>
2016-03-14 16:36:21 -0400

    Completed question 4

--- AppliedLearningAssessment/Program.cs
+++ AppliedLearningAssessment/Program.cs
@@ -27,10 +27,26 @@ namespace AppliedLearningAssessment
             double[] floatingPointNumbers = { 1.01, 2.22, 3.34, 4.56, 6.76 };
             Console.WriteLine(floatingPointNumbers[1]);
 
-            //Question 3
+            // Question 3
             foreach(double num in floatingPointNumbers)
             {
                 Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            // Question 4
+            Console.WriteLine("Enter a number (1 or 100):");
+            int switchCaseNum = int.Parse(Console.ReadLine());
+            switch (switchCaseNum)
+            {
+                case 1:
+                    Console.WriteLine("Only one?");
+                    break;
+                case 100:
+                    Console.WriteLine("100? That's a lot!");
+                    break;
+                default:
+                    Console.WriteLine("Input not recognized.");
+                    break;
             }
         }
     }
